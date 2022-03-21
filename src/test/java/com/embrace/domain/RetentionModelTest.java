0948d154c908d48e6c1c45fe776f307f9933dff2
Juan Ignacio Costamagna <juan.costamagna@mercadolibre.com>
2022-03-21 19:36:11 -0300

    Update RetentionModelTest.java

--- src/test/java/com/embrace/domain/RetentionModelTest.java
+++ src/test/java/com/embrace/domain/RetentionModelTest.java
@@ -36,6 +36,17 @@ public class RetentionModelTest {
         assertEquals(this.createRetentionModelWithUserActivity(1L, 2), retentionModel);
     }
 
+    @Test
+    public void test_add_14_consecutive_events() {
+        RetentionModel retentionModel = new RetentionModel();
+
+        for(int i = 1; i <= 14; i++) {
+            retentionModel.addActivity(i, 1L);
+        }
+
+        assertEquals(this.createRetentionModelWithUserActivity(1L, 14), retentionModel);
+    }
+
     private RetentionModel createRetentionModelWithUserActivity(long user, int consecutiveDays) {
         HashMap<Long, Integer> activities = new HashMap<>();
         activities.put(user, consecutiveDays);
@@ -63,6 +74,26 @@ public class RetentionModelTest {
         assertEquals(this.createRetentionModelWithSecondDayActivity(2L, 1), retentionModel);
     }
 
+    @Test
+    public void test_add_two_events_in_second_day() {
+        RetentionModel retentionModel = new RetentionModel();
+
+        retentionModel.addActivity(2, 2L);
+        retentionModel.addActivity(2, 2L);
+
+        assertEquals(this.createRetentionModelWithSecondDayActivity(2L, 1), retentionModel);
+    }
+
+    @Test
+    public void test_add_two_events_consecutive_in_second_day() {
+        RetentionModel retentionModel = new RetentionModel();
+
+        retentionModel.addActivity(2, 2L);
+        retentionModel.addActivity(3, 2L);
+
+        assertEquals(this.createRetentionModelWithSecondDayActivity(2L, 2), retentionModel);
+    }
+
     private RetentionModel createRetentionModelWithSecondDayActivity(long user, int consecutiveDays) {
         HashMap<Long, Integer> activities = new HashMap<>();
         activities.put(user, consecutiveDays);
@@ -78,6 +109,53 @@ public class RetentionModelTest {
         }
 
         secondDay.addNext(lastDay);
+        day.addNext(secondDay);
+
+        return new RetentionModel(day);
+    }
+
+    @Test
+    public void test_add_two_events_in_different_days() {
+        RetentionModel retentionModel = new RetentionModel();
+
+        retentionModel.addActivity(1, 2L);
+        retentionModel.addActivity(3, 2L);
+
+        assertEquals(this.createRetentionModelWithThirdDayActivity(2L, 1), retentionModel);
+    }
+
+    @Test
+    public void test_add_multiple_events_in_different_days() {
+        RetentionModel retentionModel = new RetentionModel();
+
+        retentionModel.addActivity(1, 2L);
+        retentionModel.addActivity(3, 2L);
+        retentionModel.addActivity(4, 2L);
+        retentionModel.addActivity(5, 2L);
+
+        assertEquals(this.createRetentionModelWithThirdDayActivity(2L, 3), retentionModel);
+    }
+
+    private RetentionModel createRetentionModelWithThirdDayActivity(long user, int consecutiveDays) {
+        HashMap<Long, Integer> activities = new HashMap<>();
+        activities.put(user, 1);
+
+        HashMap<Long, Integer> activitiesThirdDay = new HashMap<>();
+        activitiesThirdDay.put(user, consecutiveDays);
+
+        Day day = new Day(1, activities);
+        Day secondDay = new Day(2);
+        Day thirdDay = new Day(3, activitiesThirdDay);
+
+        DayChain lastDay = new LastDay();
+        for(int i = 14; i >= 4; i--) {
+            Day currentDay = new Day(i);
+            currentDay.addNext(lastDay);
+            lastDay = currentDay;
+        }
+
+        thirdDay.addNext(lastDay);
+        secondDay.addNext(thirdDay);
         day.addNext(secondDay);
 
         return new RetentionModel(day);
