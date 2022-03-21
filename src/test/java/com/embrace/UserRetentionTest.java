2bd40c74ff84ca0066c80a77ad6849ad605722a0
Juan Ignacio Costamagna <juan.costamagna@mercadolibre.com>
2022-03-21 00:12:57 -0300

    Create UserRetentionTest.java

---
+++ src/test/java/com/embrace/UserRetentionTest.java
@@ -0,0 +1,17 @@
+package com.embrace;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserRetentionTest {
+
+
+    @Test
+    public void test_case_00() {
+
+        UserRetention userRetention = new UserRetention();
+
+        assertEquals(1,1);
+    }
+}
