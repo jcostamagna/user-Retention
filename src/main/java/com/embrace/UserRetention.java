ea5af15fb47f59128f236f5e0750ba6557f30b21
Juan Ignacio Costamagna <juan.costamagna@mercadolibre.com>
2022-03-21 23:16:35 -0300

    Update UserRetention.java

--- src/main/java/com/embrace/UserRetention.java
+++ src/main/java/com/embrace/UserRetention.java
@@ -1,7 +1,21 @@
 package com.embrace;
 
-public class UserRetention {
-    public void process(String path) {
+import com.embrace.csvprocessor.CSVProcessor;
+import com.embrace.domain.RetentionModel;
+import com.embrace.presenters.RetentionModelPresenter;
 
+public class UserRetention {
+    private final CSVProcessor csvProcessor;
+    private final RetentionModelPresenter presenter;
+
+    public UserRetention(CSVProcessor csvProcessor, RetentionModelPresenter presenter) {
+        this.csvProcessor = csvProcessor;
+        this.presenter = presenter;
+    }
+
+    public void process(String path) {
+        RetentionModel retentionModel = this.csvProcessor.read(path);
+
+        this.presenter.present(retentionModel);
     }
 }
