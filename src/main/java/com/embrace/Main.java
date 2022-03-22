62906f36a813c9e719ff7edefd620d6886600a83
Juan Ignacio Costamagna <juan.costamagna@mercadolibre.com>
2022-03-21 23:16:43 -0300

    Update Main.java

--- src/main/java/com/embrace/Main.java
+++ src/main/java/com/embrace/Main.java
@@ -1,12 +1,20 @@
 package com.embrace;
 
+import com.embrace.csvprocessor.CSVProcessorImpl;
+import com.embrace.csvprocessor.EventUserActivityProcessor;
+import com.embrace.presenters.RetentionModelPresenterStdOut;
+
 import java.util.Arrays;
 import java.util.Optional;
 
 public class Main {
 
     public static void main(String[] args) {
-        UserRetention userRetention = new UserRetention();
+        UserRetention userRetention = new UserRetention(
+                new CSVProcessorImpl(new EventUserActivityProcessor()),
+                new RetentionModelPresenterStdOut()
+        );
+
         Optional<String> csvPath = Arrays.stream(args).findFirst();
 
         String path = csvPath.orElseThrow(() -> new IllegalArgumentException("Please provide a csv path"));
