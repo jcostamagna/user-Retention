21d12eb948da7e9f4bfcdaff816f5304169cfaa6
Juan Ignacio Costamagna <juan.costamagna@mercadolibre.com>
2022-03-22 21:41:15 -0300

    Update RetentionModelPresenterStdOut.java

--- src/main/java/com/embrace/presenters/RetentionModelPresenterStdOut.java
+++ src/main/java/com/embrace/presenters/RetentionModelPresenterStdOut.java
@@ -1,10 +1,38 @@
 package com.embrace.presenters;
 
-import com.embrace.domain.RetentionModel;
+import com.embrace.usecases.RetentionFrequenciesResponse;
+import com.embrace.usecases.RetentionModelPresenter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class RetentionModelPresenterStdOut implements RetentionModelPresenter {
+    private final int RETENTION_DAYS = 14;
+
     @Override
-    public void present(RetentionModel retentionModel) {
-        System.out.print(retentionModel.toString());
+    public void present(RetentionFrequenciesResponse retentionModel) {
+        List<HashMap<Integer, Integer>> frequencies = retentionModel.getFrequencies();
+
+        int dayNumber = 1;
+
+        for(HashMap<Integer, Integer> frequency: frequencies) {
+            System.out.print(this.presentToString(dayNumber, frequency));
+            dayNumber++;
+        }
+    }
+
+    private String presentToString(int index, HashMap<Integer, Integer> dayFrequency) {
+        StringBuffer stringBuffer = new StringBuffer();
+
+        stringBuffer.append(index);
+
+        IntStream.rangeClosed(1,RETENTION_DAYS).forEach(integer -> {
+            stringBuffer.append(",").append(dayFrequency.getOrDefault(integer, 0));
+        });
+
+        return stringBuffer
+                .append(System.lineSeparator())
+                .toString();
     }
 }
